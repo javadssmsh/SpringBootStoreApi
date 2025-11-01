0da235b5ef2cb02b1a36af6a2339beeb0112ecdf
j.shirkhani <j.shirkhani@fanap.ir>
2025-11-01 19:48:26 +0330

    return first rest api with json response

---
+++ src/main/java/com/codewithmosh/store/entities/Message.java
@@ -0,0 +1,11 @@
+package com.codewithmosh.store.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Message {
+    private String text;
+}
