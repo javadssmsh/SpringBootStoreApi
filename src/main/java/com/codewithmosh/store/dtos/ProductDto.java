46946df33ce078a30e6c377594162e37529bba12
j.shirkhani <j.shirkhani@fanap.ir>
2025-11-03 00:20:11 +0330

    add ProductController

---
+++ src/main/java/com/codewithmosh/store/dtos/ProductDto.java
@@ -0,0 +1,17 @@
+package com.codewithmosh.store.dtos;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+public class ProductDto {
+    private Long id;
+    private String name;
+    private BigDecimal price;
+    private String description;
+    private Byte categoryId;
+}
